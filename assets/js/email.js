a94941f3edd5a100bb752c3fe04c64bd5b38c6c8
T.Viet <victorpham1997@gmail.com>
2020-01-06 18:50:14 +0800

    Fixed new line on email

--- assets/js/email.js
+++ assets/js/email.js
@@ -17,13 +17,16 @@ function sendEmail() {
 	    return false;
 	}
 	Email.send({
-		SecureToken : "abae7717-9f5f-4399-9ad5-8d959de2b0c1",	    
+		SecureToken : "abae7717-9f5f-4399-9ad5-8d959de2b0c1",
+		// Host : "smtp.elasticemail.com",
+		// Username : "victorpham1997@gmail.com",
+		// Password : "657DD13E707E2A936AC6539023F408E21BC7",
 	    To : 'victorpham1997@gmail.com',
 	    From : "victorpham1997@gmail.com",
 	    Subject : "New message from " + name +" @" + email,
-	    Body : 	"Sender name: " + name <br>
-	    	+	"Sender email: " + email +"|"
-	    	+	"Subject: " + subject +"|"
+	    Body : 	"Sender name: " + name + "<br>"
+	    	+	"Sender email: " + email +"<br>"
+	    	+	"Subject: " + subject +"<br>"
 	    	+	"message: " + message
 	}).then(
 	  message => alert("Thank you for contacting me! I will reply you shortly. :)")
